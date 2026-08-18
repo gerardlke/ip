e49e8b2868d83a2f6ab79a9e78b20b7880b2adbf
gerardlke <gerardlke7@gmail.com>
2026-08-18 17:05:38 +0800

    Added ability to store whatever text user enters and displays when requested

--- src/main/java/Finn.java
+++ src/main/java/Finn.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Finn {
     public static void main(String[] args) {
+        // Greeting text
         String banner = " ____ ___ _   _ _   _ \n"
                 + "|  __|_ _| \\ | | \\ | |\n"
                 + "| |_  | ||  \\| |  \\| |\n"
@@ -15,8 +18,9 @@ public class Finn {
         String greeting = "Hello! I'm Finn.\nYour personal AI assistant!\n" + breakline;
         System.out.println(greeting);
 
-        // TODO
+        // Tools
         Scanner scanner = new Scanner(System.in);
+        List<String> commands = new ArrayList<>();
 
         while (true) {
             System.out.print("What can I do for you? ");
@@ -24,15 +28,27 @@ public class Finn {
 
             System.out.println(breakline);
 
+            // End program if command is "bye"
             if (command.equals("bye")) {
                 break;
             }
 
+            // Execute list 
+            if (command.equals("list")) {
+                for (int cur = 1; cur <= commands.size(); cur++) {
+                    System.out.println(cur + ". " + commands.get(cur - 1));
+                }
+                System.out.println(breakline);
+                continue;
+            }
+
             System.out.println("Executing: " + command);
+            commands.add(command);
 
             System.out.println(breakline);
         }
 
+        // Closing text
         String exit = "Bye. Hope to see you again soon!";
         System.out.println(exit);
     }
